76e16e16646cd4397d7a72da0e1720cdf07ea3f2
Bill Leddy <bill@williesworkshop.net>
2015-12-26 17:44:51 -0800

    Update config.php
    Remove personal information

--- config.php
+++ config.php
@@ -6,16 +6,16 @@
 date_default_timezone_set('America/Los_Angeles');
 
 // Email Settings 
-$site['from_name'] = 'Bill Leddy'; // from email name 
-$site['from_email'] = 'bill@williesworkshop.net'; // from email address 
+$site['from_name'] = 'your name'; // from email name 
+$site['from_email'] = 'you@email.net'; // from email address 
 
 // Just in case we need to relay to a different server,  
 // provide an option to use external mail server. 
 $site['smtp_mode'] = 'enabled'; // enabled or disabled 
 $site['smtp_host'] = 'smtpout.secureserver.net'; 
 $site['smtp_port'] = '80'; 
-$site['smtp_username'] = 'info@leddyconsulting.com';
-$site['smtp_password'] = 'infopass';
+$site['smtp_username'] = 'you@email.net';
+$site['smtp_password'] = 'yourpassword';
 
 $site['config_root'] = '/Users/bleddy/Sites'; // location of config.php for all sites
 $site['config_library'] = $site['config_root'].'/lib'; // location of shared libraries
